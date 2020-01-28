85cb958115dcf8fcf2655d30c9cfdb7444436173
Paweł Ładna <skorban@gmail.com>
2020-01-28 21:30:40 +0100

    With a little help from my friends

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 PROJEKT: Implementacja listy.
 
 Proszę o przesłanie implementacji listy.
-Lista musi umożliwić spis listy samochodów na parkingu. Implementacja powinna zawierać :
+Lista musi umożliwić spis listy samochodów na parkingu. 
 
-
+Implementacja powinna zawierać :
 - addNewElement -> dodanie nowego samochodu wjeżdzającego na parking
 - removeElement -> usuniecie samochodu wyjezdzajacego z parkingu
 - getNumberOfElements -> zwraca liczbę elementów listy
--- main.py
+++ main.py
@@ -1,36 +1,72 @@
+# coding=utf-8
+
 """Poniższy kod został napisany w języku Python. Jest on interpretacją lindek list, oryginalnie
     tworzonej w C. Python posiada wiele cech wspolnych z C (zbieżny syntax) i nie posiada
     mechanizmów autmatycznych dla linked list jak Java.
     Ponieważ zauważyłem że mechanizm linked list przypomina działania na obiektach, po moim
     pytaniu na wykładzie Podstawy programowaniaa, uzyskałem zgodę na użycie języka obiektowego do tego celu"""
 
-from datetime import datetime
+from datetime import datetime  # Dal używania timestampu, użyję gotowych bibliotek Pythona
 
-timestamp = 1545730073
+option = None
 
-def switch_menu(option):
-    switcher = {
-        1: "Dodaj element",
-        2: "Usuń element",
-        3: "Podaj liczbę elementów"
-    }
 
 class CarNode:
     def __init__(self, reg_numb, color, next_link=None):
-        self.time = datetime.fromtimestamp(timestamp)
-        self.regNumb = reg_numb
+        self.time = datetime.now()
+        self.reg_numb = reg_numb
         self.color = color
         self.next = next_link
 
 
-first = CarNode(datetime, 'WPR2153E', 'blue')
+class CarPark:
+    def __init__(self):
+        self.root = None
+
+    # Dodawanie na początku listy - tworzenie pierwszego node
+    def at_begining(self, reg_numb, color):
+        newCar = CarNode(reg_numb, color)
+        if self.root is not None:
+            newCar.next = self.root
+        self.root = newCar
+
+    # Dodawanie na koniec - to co nasz interesuje czyli opcja 1
+    def at_end(self, reg_numb, color):
+        newCar = CarNode(reg_numb, color)
+        if self.root is None:
+            newCar.next = self.root
+        else:
+            car = self.root
+            while car.next:
+                car = car.next
+            car.next = newCar
+
+    def remove_car(self, reg_number):  # Metoda do usuwania Node czyli opcja druga
+        car = self.root
+        if car is None:  # Ta część sprawdza tylko pierwszy element listy
+            return
+        elif car.reg_numb == reg_number:
+            self.root = car.next
+            car = None
+            return
+        while car is not None:  # Ta część iteruje listę w poszukiwaniu zgodnej rejestracji
+            if car.reg_numb == reg_number:
+                break
+            prev_car = car
+            car = car.next
+        if car is None:  # Jeśli nie ma wcale to wyjście z pętli
+            return
+        prev_car.next = car.next
+        car = None
+
+    # Opcja trzecia, drukuje listę
+    def print_list(self):
+        car = self.root
+        while car is not None:
+            print(car.reg_numb)
+            car = car.nextval
 
 
-def main():
-    if __name__ == '__main__':
-        main()
-
-
-print('Hello World! \nThis is linked list, please choose your option:')
-
-
+if __name__ == '__main__':
+    print("Hello World! \nThis is linked list, please choose your option:")
+    c1 = CarNode('WPR2153E', 'blue')
